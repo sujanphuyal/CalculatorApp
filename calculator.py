8ba9c06b911b27684b724a2ecead893cc535e4be
sujphu <sujphu@gmail.com>
2024-11-09 16:50:15 +1030

    feature-x:Modify add function

--- calculator.py
+++ calculator.py
@@ -1,6 +1,8 @@
 # calculator.py
+# Adding a new function or modifying an existing function in a way that might conflict with other branches
 def add(a, b):
-    return a + b
+    # Feature X change
+    return a + b + 1
 
 def subtract(a, b):
     return a - b
